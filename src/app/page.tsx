932afa9faf7019ca80c03eb2aab36cd346b3c645
WarFiN <82953540+WarFiN123@users.noreply.github.com>
2025-08-19 10:16:30 +0530

    Update theme-toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,12 +37,6 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { useTheme } from "next-themes";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 interface messagePayload {
   chatID?: string;
@@ -103,7 +97,7 @@ export default function TelegramTool() {
     });
   };
 
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   const saveToken = () => {
     if (!botToken || !tokenName) {
       toast.error("Error", {
@@ -669,26 +663,16 @@ export default function TelegramTool() {
             className="dark:brightness-100 brightness-0"
           />
         </Link>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <MoonStar className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle theme</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>
-              System
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() =>
+            theme === "light" ? setTheme("dark") : setTheme("light")
+          }
+        >
+          <Sun className="dark:hidden block" />
+          <MoonStar className="dark:block hidden" />
+        </Button>
       </footer>
     </div>
   );
